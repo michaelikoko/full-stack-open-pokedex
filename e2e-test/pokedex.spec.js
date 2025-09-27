b8274476c05de87c07f179367bf32964aa14ebff
Michael Ikoko <michaelikoko.o@gmail.com>
2025-09-27 23:45:45 +0100

    Wrote test that ensures navigation from main page to page of particular Pokemon

--- e2e-test/pokedex.spec.js
+++ e2e-test/pokedex.spec.js
@@ -10,4 +10,15 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+
+  test('navigating to a pokemon page', async ({ page }) => {
+    await page.goto('http://localhost:5000')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()  })
 })
