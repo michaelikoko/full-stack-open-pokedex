33197626d476e29c9be941253fc91c8d912de25a
Michael Ikoko <michaelikoko.o@gmail.com>
2025-10-03 22:21:28 +0100

    Added healthcheck workflow

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('Pokedex app version 3') // change this string to ensure a new version deployed
+  res.send('Pokedex app version 4') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
